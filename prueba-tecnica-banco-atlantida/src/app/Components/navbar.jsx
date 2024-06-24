a17a932ee786b4a666ed725d7c041481ff4a5ee0
christian-lopezxd <00179320@uca.edu.sv>
2024-06-24 01:54:52 -0600

    :sparkles: add: NavBar

---
+++ prueba-tecnica-banco-atlantida/src/app/Components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='w-full bg-primary h-[10vh] flex flex-row items-center'>
+        <img src='/logo.png' className='max-h-[10vh]' />
+        
+    </div>
+  )
+}
+
+export default NavBar
--- prueba-tecnica-banco-atlantida/src/app/page.js
+++ prueba-tecnica-banco-atlantida/src/app/page.js
@@ -1,3 +1,11 @@
+import NavBar from './Components/navbar'
+
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return <section className="text-black font-bold flex flex-col gap-3">
+    <NavBar />
+   <div className='flex flex-col px-3 text-2xl'>
+   <h1>Listado de Tarjetas</h1>
+   </div>
+  </section>
 }
